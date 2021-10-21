dfe51bdc5b7a966218fb30dd0a1b7affadae306d
zahrulsch <mantr4code@gmail.com>
2021-10-21 14:28:15 +0700

    Adding axios timeout

--- apis/axios.js
+++ apis/axios.js
@@ -2,12 +2,12 @@ const axios = require('axios')
 
 const jikan = axios.create({
   baseURL: 'https://api.jikan.moe/v3',
-  timeout: 3000
+  timeout: 6000
 })
 
 const mangadex = axios.create({
   baseURL: 'https://api.mangadex.org',
-  timeout: 3000
+  timeout: 6000
 })
 
 const youtube = axios.create({
